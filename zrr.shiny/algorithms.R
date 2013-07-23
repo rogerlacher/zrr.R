0ce716c2050ee4c15f26f7a5334b278cfd4a421d
rogerlacher <roger.lacher@alumni.ethz.ch>
2013-07-24 00:01:19 +0200

    Added S&P and Quakes visualizations. Need to do some knitr-reporting, what-if and performance.

---
+++ zrr.shiny/algorithms.R
@@ -0,0 +1,213 @@
+# MDM and other ZRR algorithms in R
+#
+# Created:  30.10.2012      Roger Lacher
+# Revision:  22.07.2013      Roger Lacher
+#            optimize performance of algos in regards to display
+#            as a Google Motion chart via shiny/rgoogleVis
+# =======================================
+#
+# rMDM Calculations:
+# 
+# Let:
+#     r(k,i)
+#         be a Matrix of Risk Values for a set of Entities
+#         where k=1..m is the number of risks
+#               i=1..n is the number of entities (countries)
+#               r(k,i) ??? [0..1] for all k,i
+#   
+#     R(i) a shorthand notation for (r(1,i),r(2,i),....r(m,i))
+#
+#
+#     p   be a vector of indices with dim(p) < m
+#                 e.g. p = c(2,5,6,7,8)
+#     q   be a vector of indices with dim(q) <= (m-p)
+#                 e.g. q = c()
+#         and p ^ q = 0 (no overlapping elements)
+#     1??  a 1-vector of dimension m
+#     0??  a 0-vector of dimension m
+#
+#   [Remark: Directly implement p,q as vectors of strings, i.e. risk names?]
+#    
+#     
+# Then:
+#     E(x)=1??[p]      a 1-vector for the MDM x-projection
+#                             of r(k,i) with dimension p < m
+#     E(y)=1??[q]      a 1-vector for the MDM y-projection
+#                             of r(k,i) with dimension q <= (m-p)
+#     E(A)=1??[c(p,q)] a 1-vector for the MDM projection of the
+#                             Armaggeddon point with dimension p+q
+#     E(N)=0??[c(p,q)] a 0-vector for the MDM projection of the
+#                             Nirvana point with dimension p+q
+#
+#
+#     Ln(i)= ||R(i)||       = sum(R(i)^2)
+#     La(i)= ||R(i)-E(A)||  = sum((R(i)-E(A))^2)
+#     Lx(i)= ||R(i)-E(x)||  = sum((R(i)-E(x))^2)
+#     Ly(i)= ||R(i)-E(y)||  = sum((R(i)-E(y))^2)
+#
+#     h(i) = 0.5*(Ln(i)-Ly(i)+||E(y)||^2)/||E(y)||
+#     g(i) = 0.5*(Ly(i)-La(i)+||E(x)||^2)/||E(x)||
+#
+#     u(i) = 0.5 * (-sqrt(Lx(i)-h(i)^2)+sqrt(Ln(i)-h(i)^2) + ||E(x)||^2)/||E(x)||
+#     v(i) = 0.5 * (-sqrt(Ly(i)-g(i)^2)+sqrt(Ln(i)-g(i)^2) + ||E(y)||^2)/||E(y)||
+#
+# ================================================================================
+
+# returns the mdm that can be displayed in a 
+# Google Visualization MotionChart with r-GoogleVis
+# Structure:
+#   Name    Year  Region  qx    qy    overlayData   Date
+#   Sambia  2008  Africa  0.4   0.65  2.3 M (USD)   2008-11-01
+
+rMDM_Motion <- function(r,p,q,sF=0.1) {
+  # TODO: implement this properly
+  # for now:
+  mdm <- rMDM(r,p,q,sF=0.1)
+  return(cbind(Country.Name=mdm[,"Country.Name"],Date=as.Date(mdm[,"Date"]),Continent=mdm[,"Continent"],mdm[,c("qx","qy")]))
+}
+
+# calculcate the rMDM of a matrix of country coordinates
+#   params:   r = matrix of countries (rows) and risk values (cols)
+#             p = index-vector (numeric) for MDM x-projection
+#             q = index-vector (numeric) for MDM y-projection
+#             sF = scaling factor (defaults to 0.1)
+#   returns:  c(u,v) = rMDM projection coordinates
+# ==================================================================
+rMDM <- function(r,p,q,sF=0.1) {
+  ret <- vector();  
+  if (is.vector(r)) {
+    ret <- rbind(ret,rMDMOneEntity(r,p,q));
+  } else {
+    for (i in 1:nrow(r)) {
+      ret <- rbind(ret,rMDMOneEntity(r[i,],p,q));
+    }        
+  }
+  # rescale  for [0.1 ... 0.9]
+  ret[,1] = sF + (1-2*sF) * (ret[,1] - min(ret[,1])) / (max(ret[,1]) - min(ret[,1]))
+  ret[,2] = sF + (1-2*sF) * (ret[,2] - min(ret[,2])) / (max(ret[,2]) - min(ret[,2]))  
+  colnames(ret) <- c("qx","qy");
+  index <- match(c("Rising.food.price"),colnames(r)) - 1;
+  ret <- cbind(r[,1:index],ret);
+}
+
+
+# calculcate the rMDM position of 1 entity (one row of the matrix)
+#   params:   r = point coordinates in m-dim space (a matrix or vector)
+#             p = index-vector (numeric) for MDM x-projection
+#             q = index-vector (numeric) for MDM y-projection
+#   returns:  c(u,v) = rMDM projection coordinates
+# ==================================================================
+rMDMOneEntity <- function(r,p,q) {
+  
+  T <- 10^-8;
+  
+  r_ <- r[c(p,q)];
+  sx = sqrt(length(p));
+  sy = sqrt(length(q));
+  
+  ln <- l2(r_);
+  la <- l2(r_ - 1);
+  lx <- l2(r[p] - 1) + l2(r[q]);
+  ly <- l2(r[q] - 1) + l2(r[p]);
+  
+  h <- 0.5*(ln-ly+sy*sy)/sy;
+  g <- 0.5*(ly-la+sx*sx)/sx;
+  
+  u <- 0.5*(-sqrt(T+lx-h*h) + sqrt(T+ln-h*h) + sx)/sx;
+  v <- 0.5*(-sqrt(T+ly-g*g) + sqrt(T+ln-g*g) + sy)/sy;
+  
+  ret <- c(u,v);
+  ret
+}
+
+
+# calculates the squared euclidian norm of a vector v
+# ===================================================
+l2 <- function(v) {
+  ret <- sum(v^2)
+  ret
+}
+
+
+
+# ==================================================================
+
+# creates a labelled scatter plot of the MDM values
+#   params:   mdm = a calculated mdm (return value from rMDM() function)
+#             xLabel, yLabel = alternate labels for x/y axes
+# ==================================================================
+plotMDM <- function(mdm, xLabel="qx", yLabel="qy") {
+  x <- mdm[,"qx"];
+  y <- mdm[,"qy"];       
+  plot(x,y, main="ZRR rMDM Plot",asp=1, xlim=c(0,1), ylim=c(0,1), 
+       xlab=xLabel, ylab=yLabel);
+  textxy(x,y,rownames(mdm));
+}
+
+
+# Creates boxplots for the wall risks
+#   params:   r = a matrix of risks to boxplot
+#             label = label for the plot
+# ==================================================================
+plotWall <- function(r,label="Wall Risks") {
+  par(mai=par("mai")+c(1,0,0,0));    # increase the bottom margin
+  # to make room for the labels
+  boxplot(r,las=3,main=label);
+  
+  # with risk values for a given country (4) overlayed:
+  # plot(p,r[4,p],xlab="",xaxt='n',ylim=c(0,1))
+  # boxplot(r[,p],las=3,main="Economic Risks", add=TRUE)
+  
+  # or, with good results for length(p) > ~12:
+  # boxplot(r[,p],las=3,main="Economic Risks",at=seq(0.5,length(p)+0.5,by=(length(p)+1)/length(p)))
+  # par(new=TRUE)
+  # plot(p,r[4,p],xlab="",xaxt='n',ylim=c(0,1))
+  
+}
+
+draw <- function(panel,mdm, xLabel="qx", yLabel="qy") {
+  x <- mdm[,1];
+  y <- mdm[,2];       
+  plot(x,y, main="ZRR rMDM Plot",asp=1, xlim=c(0,1), ylim=c(0,1), 
+       xlab=xLabel, ylab=yLabel);
+  textxy(x,y,rownames(mdm));
+  panel;
+}
+
+
+# ==================================================================
+# 
+# test function
+test_MDM <- function() {  
+  
+  NCOL <- 8
+  
+  r <- read.csv("data/countryrisks.csv");  
+  
+    
+  # randomly subset for some 10 countries only
+  r <- r[sample(1:nrow(r),10,replace=FALSE),]
+    
+  # get rid of columns only containing NAs (i.e. risk categories)
+  r <- r[,colSums(is.na(r))<nrow(r)];
+  
+  # some random risk frame
+  # filter out the categories & first NCOL columns
+  #c_ex <- c(1:NCOL,match(names(r[colSums(is.na(r)) > 10]),colnames(r)))
+  x <- 1:ncol(r)
+  #x <- x[-c_ex]
+  x <- x[-c(1:NCOL)]
+  p <- sample(x, 15, replace = F);    # x-projection indices, 15 dimensions
+  q <- sample(x[-p], 10, replace = F);    # y-projection indices, 10 dimensions
+  
+  mdm <- rMDM_Motion(r,p,q);
+  Motion=gvisMotionChart(mdm, idvar="Country.Name", timevar="Date", options=list(height=600, width=800)) # Display chart plot(Motion)  
+  plot(Motion)
+  #plotMDM(mdm, xLabel="My x Risks", yLabel="My y Risks");
+  mdm;
+}
+# 
+# 
+# # library "googleVis" is required for the textxy labeling in the test function
+library("googleVis");
+library("calibrate");
---
+++ zrr.shiny/global.R
@@ -0,0 +1,51 @@
+library(XML)
+# S&P Ratings 
+url <- "http://en.wikipedia.org/wiki/List_of_countries_by_credit_rating"
+x <- readHTMLTable(readLines(url), which=2)
+levels(x$Rating) <- substring(levels(x$Rating), 4, 
+                              nchar(levels(x$Rating)))
+x$Ranking <- x$Rating
+levels(x$Ranking) <- nlevels(x$Rating):1
+x$Ranking <- as.character(x$Ranking)
+x$Rating <- paste(x$Country, x$Rating, sep=": ")
+
+# recent earthquakes
+eq <- read.csv("http://earthquake.usgs.gov/earthquakes/feed/v0.1/summary/2.5_week.csv")
+eq$loc=paste(eq$Latitude, eq$Longitude, sep=":")
+
+# global risk dataset - only read and process once
+
+r<-read.csv("../data/countryrisks.csv")
+# restrict to 1 timestamp
+r1 <- subset(r,Date == r[1,"Date"])
+countries <- r1[,c("Country.Name","Code")]
+
+# identify the risk categories
+# constructs a hierarchical list of risk categories and associated risks
+# TODO: need to define a sensible limits for the number of NA's
+rcats <- names(r[colSums(is.na(r)) == nrow(r)])
+rh <- list(rcats)
+# some change here
+rc1 <- rcats[1]
+for (rc2 in rcats[-1]) {
+  pos1 <- match(rc1,colnames(r)) + 1
+  pos2 <- match(rc2,colnames(r)) - 1
+  rh[[rc1]] <- list(colnames(r[,pos1:pos2]))
+  rc1 <- rc2
+}
+pos1 <- match(rc1,colnames(r)) + 1
+pos2 <- length(colnames(r))
+rh[[rc1]] <- list(colnames(r[,pos1:pos2]))
+
+# identify the risk names 
+rnames <- unlist(rh,use.names=FALSE)
+rnames <- rnames[(length(rcats)+1):length(rnames)]
+
+
+# include the calculation algorithms
+source(file = "algorithms.R")
+# using the compiled algorithms version will speed up things
+# loadcmp(file="algorithms.R.c")
+
+# turn on profiling
+#Rprof(filename="Rprof.out",append=FALSE,interval=0.02,memory.profiling=FALSE)
