fd772028532a87d74b560bb02407a58ed7bbfedd
U-EZCORP\CHL9804 <CHL9804@NCHA25501275.emea.zurich.corp>
2013-09-20 17:44:55 +0200

    commit so nothing gets lost

--- zrr.shiny.dev/server.R
+++ zrr.shiny.dev/server.R
@@ -1,65 +1,136 @@
 require(rCharts)
+require(stringr)
 library(shiny)
 
 # Define server logic required to generate and plot a random distribution
 shinyServer(function(input, output) {
   
-  output$polyChart <- renderChart({
-    names(iris) = gsub("\\.", "", names(iris))
-    p1 <- rPlot(input$x, input$y, data = iris, color = "Species", 
-                facet = "Species", type = 'point')
-    p1$addParams(dom = 'polyChart')
-    return(p1)
+  # query the risk values using filters countries, risks, period
+  getRiskValues <- reactive({
+    cIds <- subset(countries, GEO_NAME %in% input$countries, select=GEO_UNIT_ID)
+    cIds <- paste(cIds[,1], collapse=",")    
+    xrIds <- subset(risknames, INDICATOR_NAME %in% input$xRisks, select=INDICATOR_ID)
+    xrIds <- paste(xrIds[,1],collapse=",")
+    yrIds <- subset(risknames, INDICATOR_NAME %in% input$yRisks, select=INDICATOR_ID)
+    yrIds <- paste(yrIds[,1],collapse=",")
+    period <- periods[input$period,][,"PERIOD_ID"]
+    
+    query <- paste("SELECT FK_INDICATOR, FK_GEO_UNIT, INDICATOR_VALUE, 'x' as 'WALL' ",
+                   "FROM RR_INDICATOR_VALUE",tblext[envId]," ",
+                   "WHERE FK_TIME_PERIOD = ",period," ",
+                   "AND FK_INDICATOR IN (", as.character(xrIds),") ", 
+                   "AND FK_GEO_UNIT IN (", as.character(cIds), ") UNION ",
+                   "SELECT FK_INDICATOR, FK_GEO_UNIT, INDICATOR_VALUE, 'y' as 'WALL' ",
+                   "FROM RR_INDICATOR_VALUE",tblext[envId]," ",
+                   "WHERE FK_TIME_PERIOD = ",period," ",
+                   "AND FK_INDICATOR IN (", as.character(yrIds),") ", 
+                   "AND FK_GEO_UNIT IN (", as.character(cIds), ")",sep="")    
+    
+    browser()
+    results <- sqlQuery(con,query)
+    results <- merge(results,countries,by.x="FK_GEO_UNIT",by.y="GEO_UNIT_ID")[,c("FK_GEO_UNIT","GEO_NAME","FK_INDICATOR","INDICATOR_VALUE","WALL")]
+    results <- merge(results,risknames,by.x="FK_INDICATOR",by.y="INDICATOR_ID")[,c("FK_GEO_UNIT","FK_INDICATOR","GEO_NAME","INDICATOR_NAME","INDICATOR_VALUE","WALL")]
+    return(results)
   })
+  
+  
+  output$mdmPlot <- renderChart({
+  })
+  
+  
+  
+  output$xRiskWall <- renderChart({    
+    r <- getRiskValues();
 
-  output$highChart <- renderChart({
-    names(iris) = gsub("\\.", "", names(iris))
-    p1 <- rCharts::Highcharts$new()
-    sapply(1:10,function(x) {
-      p1$series(data=rnorm(10), draggableY = TRUE)
-    })
-    p1$plotOptions(
+    p1 <- hPlot(x="INDICATOR_NAME",y="INDICATOR_VALUE",
+                data=subset(r,WALL=="x"), type="line",group="GEO_NAME", title="x Risks");
+    p1$series(draggableY=TRUE)
+    p1$plotOptions(        
       series = list(
         cursor = "ns-resize",
         point = list(
           events = list(
-            drag = "#! function() {  $('#drag').html( 
-                            'Dragging <b>' + this.series.name + '</b>, <b>' +
-                            this.category + '</b> to <b>' + 
-                            Highcharts.numberFormat(e.newY, 2) + '</b>'
-                        ); } !#",
             drop = "#! function() { whatif(this); } !#" )
-        # TODO: need to figure out how to hand over the delta between starting / end position
-        #       possibly best to adapt the draggable-points.js 
-#            drop = "#! function() { $('#drop').html(
-#                            'In <b>' + this.series.name + '</b>, <b>' +
-#                            this.category + '</b> was set to <b>' + 
-#                            Highcharts.numberFormat(this.y, 2) + '</b>'
-#                        ); } !#"  )
-         ),
+        ),
         stickyTracking = TRUE
       )
-    )
-    #p1 <- hPlot(input$x, input$y, data = iris, type = c("line", "bubble"), group = "Species", size=1)
-    p1$addParams(dom = 'highChart')
-    return(p1)
+    );    
+    p1$addParams(dom = 'xRiskWall');
+    return(p1);
+        
   })
   
-  # Expression that generates a plot of the distribution. The expression
-  # is wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should be automatically 
-  #     re-executed when inputs change
-  #  2) Its output type is a plot 
-  #
-  output$distPlot <- renderPlot({
+  
+  output$yRiskWall <- renderChart({    
+    r <- getRiskValues();
     
-    # generate an rnorm distribution and plot it
-    dist <- rnorm(input$obs)
-    hist(dist)
+    p1 <- hPlot(x="INDICATOR_NAME",y="INDICATOR_VALUE",
+                data=subset(r,WALL=="y"), type="line",group="GEO_NAME", title="y Risks");    
+    p1$plotOptions(        
+      series = list(
+        cursor = "ns-resize",
+        point = list(
+          events = list(
+            drop = "#! function() { whatif(this); } !#" )
+        ),
+        stickyTracking = TRUE
+      )
+    );    
+    p1$addParams(dom = 'yRiskWall');
+    return(p1);
+
+    })
+  
+  
+#   output$xRiskWall <- renderChart({
+#     names(iris) = gsub("\\.", "", names(iris))
+#     p1 <- rCharts::Highcharts$new()
+#     sapply(1:10,function(x) {
+#       p1$series(data=rnorm(10), draggableY = TRUE)
+#     })
+#     p1$plotOptions(
+#       series = list(
+#         cursor = "ns-resize",
+#         point = list(
+#           events = list(
+# #            drag = "#! function() {  $('#drag').html( 
+# #                            'Dragging <b>' + this.series.name + '</b>, <b>' +
+# #                            this.category + '</b> to <b>' + 
+# #                            Highcharts.numberFormat(e.newY, 2) + '</b>'
+# #                        ); } !#",
+#             drop = "#! function() { whatif(this); } !#" )
+#         # TODO: need to figure out how to hand over the delta between starting / end position
+#         #       possibly best to adapt the draggable-points.js 
+# #            drop = "#! function() { $('#drop').html(
+# #                            'In <b>' + this.series.name + '</b>, <b>' +
+# #                            this.category + '</b> was set to <b>' + 
+# #                            Highcharts.numberFormat(this.y, 2) + '</b>'
+# #                        ); } !#"  )
+#          ),
+#         stickyTracking = TRUE
+#       )
+#     )
+#     #p1 <- hPlot(input$x, input$y, data = iris, type = c("line", "bubble"), group = "Species", size=1)
+#     p1$addParams(dom = 'xRiskWall')
+#     return(p1)
+#   })
+  
+  
+  # conditionally create output widgets for xRisks and yRisks
+  # based on the xRiskCategory and yRiskCategory selections
+  output$xRisks <- renderUI({
+    riskSelection <- unlist(subset(risknames,
+                                   INDICATOR_CATEGORY==input$xRiskCategory,
+                                   select=INDICATOR_NAME),use.names=FALSE)
+    selectInput("xRisks", "Choose x Risk:", 
+                choices = riskSelection, multiple=TRUE)      
   })
   
-  output$echoDrag <- renderPrint({
-    print(input$drag)
+  output$yRisks<- renderUI({
+    riskSelection <- unlist(subset(risknames,
+                                   INDICATOR_CATEGORY==input$yRiskCategory,
+                                   select=INDICATOR_NAME),use.names=FALSE)
+    selectInput("yRisks", "Choose y Risk:", 
+                choices = riskSelection, multiple=TRUE)      
   })
 })
--- zrr.shiny.dev/ui.R
+++ zrr.shiny.dev/ui.R
@@ -6,42 +6,47 @@ includeDraggableJS <- function() {
   tagList(tags$head(
     #tags$script(src = "https://rawgithub.com/highslide-software/draggable-points/master/draggable-points.js"),
     tags$script(src = "js/draggable-points.js"),
-    tags$script(src = "js/drag.js"),
+    #tags$script(src = "js/drag.js"),
     tags$script(src = "js/whatif.js")
   ))
 }
+
 
 # Define UI for application that plots random distributions 
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("Hello Shiny!"),
+  headerPanel("Zurich Risk Room Prototype"),
   
   # Sidebar with a slider input for number of observations
   sidebarPanel(
-    sliderInput("obs", 
-                "Number of observations:", 
-                min = 1,
-                max = 1000, 
-                value = 500),
-    selectInput(inputId = "x",
-                label = "Choose X",
-                choices = c('SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'),
-                selected = "SepalLength"),
-    selectInput(inputId = "y",
-                label = "Choose Y",
-                choices = c('SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'),
-                selected = "SepalWidth"),
+    selectInput("countries", "Choose your Countries:", 
+                choices = countries[,"GEO_NAME"], multiple=TRUE),
+    
+    selectInput("xRiskCategory", "Choose x risks Category:", 
+                choices = riskcats),
+    uiOutput("xRisks"),
+    
+    selectInput("yRiskCategory", "Choose y risks Category:", 
+                choices = riskcats),
+    uiOutput("yRisks"),
+    
+    sliderInput("period", "Choose Time:", 
+                min=1,
+                max=length(periods[,"PERIOD"]),
+                value=1,
+                step=1),
+    
     tag("div",list(id="drag", class="drag"))
   ),
   
   # Show a plot of the generated distribution
   mainPanel(
     tabsetPanel(
-      tabPanel("DistPlot", plotOutput("distPlot")),
-      tabPanel("rChart",showOutput("polyChart", "polycharts")),
-      tabPanel("rChart",showOutput("highChart", "highcharts")),
-      tabPanel("Echo",verbatimTextOutput("echoDrag"))
+      tabPanel("MDM Plot", showOutput("mdmPlot","Highcharts")),
+      tabPanel("x Risk Walls",showOutput("xRiskWall", "Highcharts")),
+      tabPanel("y Risk Walls",showOutput("yRiskWall", "Highcharts")),
+      tabPanel("Values Table", tableOutput("table"))
       ),
     includeDraggableJS()
   )
