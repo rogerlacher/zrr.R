8a49a222210c0ddfbf026fa9cd2962ba040289f6
U-EZCORP\CHL9804 <CHL9804@NCHA25501275.emea.zurich.corp>
2013-09-22 08:10:58 +0200

    csv-based dev environment added

--- zrr.shiny.dev/server.R
+++ zrr.shiny.dev/server.R
@@ -1,32 +1,39 @@
 require(rCharts)
 require(stringr)
 library(shiny)
+library(plyr)
+
+
 
 # Define server logic required to generate and plot a random distribution
 shinyServer(function(input, output) {
   
   # query the risk values using filters countries, risks, period
   getRiskValues <- reactive({
-    cIds <- subset(countries, GEO_NAME %in% input$countries, select=GEO_UNIT_ID)
-    cIds <- paste(cIds[,1], collapse=",")    
-    xrIds <- subset(risknames, INDICATOR_NAME %in% input$xRisks, select=INDICATOR_ID)
-    xrIds <- paste(xrIds[,1],collapse=",")
-    yrIds <- subset(risknames, INDICATOR_NAME %in% input$yRisks, select=INDICATOR_ID)
-    yrIds <- paste(yrIds[,1],collapse=",")
+#     cIds <- subset(countries, GEO_NAME %in% input$countries, select=GEO_UNIT_ID)
+#     cIds <- paste(cIds[,1], collapse=",")    
+#     xrIds <- subset(risknames, INDICATOR_NAME %in% input$xRisks, select=INDICATOR_ID)
+#     xrIds <- paste(xrIds[,1],collapse=",")
+#     yrIds <- subset(risknames, INDICATOR_NAME %in% input$yRisks, select=INDICATOR_ID)
+#     yrIds <- paste(yrIds[,1],collapse=",")
     period <- periods[input$period,][,"PERIOD_ID"]
     
-    query <- paste("SELECT FK_INDICATOR, FK_GEO_UNIT, INDICATOR_VALUE, 'x' as 'WALL' ",
+#     query <- paste("SELECT FK_INDICATOR, FK_GEO_UNIT, INDICATOR_VALUE, 'x' as WALL ",
+#                    "FROM RR_INDICATOR_VALUE",tblext[envId]," ",
+#                    "WHERE FK_TIME_PERIOD = ",period," ",
+#                    "AND FK_INDICATOR IN (", as.character(xrIds),") ", 
+#                    "AND FK_GEO_UNIT IN (", as.character(cIds), ") UNION ",
+#                    "SELECT FK_INDICATOR, FK_GEO_UNIT, INDICATOR_VALUE, 'y' as WALL ",
+#                    "FROM RR_INDICATOR_VALUE",tblext[envId]," ",
+#                    "WHERE FK_TIME_PERIOD = ",period," ",
+#                    "AND FK_INDICATOR IN (", as.character(yrIds),") ", 
+#                    "AND FK_GEO_UNIT IN (", as.character(cIds), ")",sep="")    
+ 
+    # First: select all
+    query <- paste("SELECT FK_INDICATOR, FK_GEO_UNIT, INDICATOR_VALUE ",
                    "FROM RR_INDICATOR_VALUE",tblext[envId]," ",
-                   "WHERE FK_TIME_PERIOD = ",period," ",
-                   "AND FK_INDICATOR IN (", as.character(xrIds),") ", 
-                   "AND FK_GEO_UNIT IN (", as.character(cIds), ") UNION ",
-                   "SELECT FK_INDICATOR, FK_GEO_UNIT, INDICATOR_VALUE, 'y' as 'WALL' ",
-                   "FROM RR_INDICATOR_VALUE",tblext[envId]," ",
-                   "WHERE FK_TIME_PERIOD = ",period," ",
-                   "AND FK_INDICATOR IN (", as.character(yrIds),") ", 
-                   "AND FK_GEO_UNIT IN (", as.character(cIds), ")",sep="")    
+                   "WHERE FK_TIME_PERIOD = ",period ,sep="")
     
-    browser()
     results <- sqlQuery(con,query)
     results <- merge(results,countries,by.x="FK_GEO_UNIT",by.y="GEO_UNIT_ID")[,c("FK_GEO_UNIT","GEO_NAME","FK_INDICATOR","INDICATOR_VALUE","WALL")]
     results <- merge(results,risknames,by.x="FK_INDICATOR",by.y="INDICATOR_ID")[,c("FK_GEO_UNIT","FK_INDICATOR","GEO_NAME","INDICATOR_NAME","INDICATOR_VALUE","WALL")]
@@ -35,6 +42,8 @@ shinyServer(function(input, output) {
   
   
   output$mdmPlot <- renderChart({
+#    p1 <- Highcharts$new()
+#    return(p1)
   })
   
   
@@ -42,19 +51,9 @@ shinyServer(function(input, output) {
   output$xRiskWall <- renderChart({    
     r <- getRiskValues();
 
-    p1 <- hPlot(x="INDICATOR_NAME",y="INDICATOR_VALUE",
+    p1 <- riskWallPlot(x="INDICATOR_NAME",y="INDICATOR_VALUE",
                 data=subset(r,WALL=="x"), type="line",group="GEO_NAME", title="x Risks");
-    p1$series(draggableY=TRUE)
-    p1$plotOptions(        
-      series = list(
-        cursor = "ns-resize",
-        point = list(
-          events = list(
-            drop = "#! function() { whatif(this); } !#" )
-        ),
-        stickyTracking = TRUE
-      )
-    );    
+  
     p1$addParams(dom = 'xRiskWall');
     return(p1);
         
@@ -63,57 +62,13 @@ shinyServer(function(input, output) {
   
   output$yRiskWall <- renderChart({    
     r <- getRiskValues();
-    
-    p1 <- hPlot(x="INDICATOR_NAME",y="INDICATOR_VALUE",
+    p1 <- riskWallPlot(x="INDICATOR_NAME",y="INDICATOR_VALUE",
                 data=subset(r,WALL=="y"), type="line",group="GEO_NAME", title="y Risks");    
-    p1$plotOptions(        
-      series = list(
-        cursor = "ns-resize",
-        point = list(
-          events = list(
-            drop = "#! function() { whatif(this); } !#" )
-        ),
-        stickyTracking = TRUE
-      )
-    );    
+
     p1$addParams(dom = 'yRiskWall');
     return(p1);
 
     })
-  
-  
-#   output$xRiskWall <- renderChart({
-#     names(iris) = gsub("\\.", "", names(iris))
-#     p1 <- rCharts::Highcharts$new()
-#     sapply(1:10,function(x) {
-#       p1$series(data=rnorm(10), draggableY = TRUE)
-#     })
-#     p1$plotOptions(
-#       series = list(
-#         cursor = "ns-resize",
-#         point = list(
-#           events = list(
-# #            drag = "#! function() {  $('#drag').html( 
-# #                            'Dragging <b>' + this.series.name + '</b>, <b>' +
-# #                            this.category + '</b> to <b>' + 
-# #                            Highcharts.numberFormat(e.newY, 2) + '</b>'
-# #                        ); } !#",
-#             drop = "#! function() { whatif(this); } !#" )
-#         # TODO: need to figure out how to hand over the delta between starting / end position
-#         #       possibly best to adapt the draggable-points.js 
-# #            drop = "#! function() { $('#drop').html(
-# #                            'In <b>' + this.series.name + '</b>, <b>' +
-# #                            this.category + '</b> was set to <b>' + 
-# #                            Highcharts.numberFormat(this.y, 2) + '</b>'
-# #                        ); } !#"  )
-#          ),
-#         stickyTracking = TRUE
-#       )
-#     )
-#     #p1 <- hPlot(input$x, input$y, data = iris, type = c("line", "bubble"), group = "Species", size=1)
-#     p1$addParams(dom = 'xRiskWall')
-#     return(p1)
-#   })
   
   
   # conditionally create output widgets for xRisks and yRisks
@@ -134,3 +89,104 @@ shinyServer(function(input, output) {
                 choices = riskSelection, multiple=TRUE)      
   })
 })
+
+# ========================= Functions -> store in different file =====================#
+# Plot Risk Room Wall using HighCharts
+# Plot includes boxplots & draggableY event listener for "what-if"
+# What-if calculations implemented in javascript -> cf. file "whatif.js"
+#
+riskWallPlot <- function(..., radius = 3, title = NULL, subtitle = NULL, group.na = NULL){
+  rChart <- Highcharts$new()
+  
+  # Get layers
+  d <- getLayer(...)
+  
+  data <- data.frame(
+    x = d$data[[d$x]],
+    y = d$data[[d$y]]
+  )
+  
+  if (!is.null(d$group)) {
+    data$group <- as.character(d$data[[d$group]])
+    if (!is.null(group.na)) {
+      data$group[is.na(data$group)] <- group.na
+    }
+  }
+  if (!is.null(d$size)) data$size <- d$data[[d$size]]
+  
+  nrows <- nrow(data)
+  data <- na.omit(data)  # remove remaining observations with NA's
+  
+  if (nrows != nrow(data)) warning("Observations with NA has been removed")
+  
+  data <- data[order(data$x, data$y), ]  # order data (due to line charts)
+  
+  if ("bubble" %in% d$type && is.null(data$size)) stop("'size' is missing")
+  
+  if (!is.null(d$group)) {
+    groups <- sort(unique(data$group))
+    types <- rep(d$type, length(groups))  # repeat types to match length of groups
+    
+    plyr::ddply(data, .(group), function(x) {
+      g <- unique(x$group)
+      i <- which(groups == g)
+      
+      x$group <- NULL  # fix
+      rChart$series(
+        data = toJSONArray2(x, json = F, names = F),
+        name = g,
+        type = types[[i]],
+        marker = list(radius = radius),
+        draggableY = TRUE
+      )
+      return(NULL)
+    })
+  } else {
+    
+    rChart$series(
+      data = toJSONArray2(data, json = F, names = F),
+      type = d$type[[1]],
+      marker = list(radius = radius),
+      draggableY = TRUE
+    )
+    
+    rChart$legend(enabled = FALSE)
+  }
+  
+  # Fix defaults
+  
+  ## xAxis
+  if (is.categorical(data$x)) {
+    rChart$xAxis(title = list(text = d$x), categories = unique(as.character(data$x)), replace = T)
+  } else {
+    rChart$xAxis(title = list(text = d$x), replace = T)
+  }
+  
+  ## yAxis
+  if (is.categorical(data$y)) {
+    rChart$yAxis(title = list(text = d$y), categories = unique(as.character(data$y)), replace = T)
+  } else {
+    rChart$yAxis(title = list(text = d$y), replace = T)
+  }
+  
+  ## title
+  rChart$title(text = title, replace = T)
+  
+  ## subtitle
+  rChart$subtitle(text = subtitle, replace = T)
+  
+  
+  ## what-if bindings
+  rChart$plotOptions(        
+    series = list(
+      cursor = "ns-resize",
+      point = list(
+        events = list(
+          drop = "#! function() { whatif(this); } !#" )
+      ),
+      stickyTracking = TRUE
+    )
+  );  
+  
+  return(rChart$copy())
+}
