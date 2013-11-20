e0d88a85dec1aa1092823266d976550039ce2f94
rogerlacher <roger.lacher@alumni.ethz.ch>
2013-11-21 00:27:56 +0100

    Commiting global.R

---
+++ zrr.shiny.dev/global.R
@@ -0,0 +1,24 @@
+library(RODBC)
+library(reshape2)
+
+
+# Source the DB configuration
+source("dbconf.R")
+
+# Current environment
+env    <- "dev"
+envId  <- envs[[env]]
+con    <- odbcConnect(dsn[envId],uid=user[envId],pwd=pass[envId])
+
+# Does the same thing as sqlFetch, however, sqlFetch does not work with
+# the ODBC text driver on text (.csv) files
+#
+mSqlFetch <- function(con,tblname,ext="") {  
+  return(sqlQuery(con,paste("SELECT * FROM ",tblname, ext,sep="")));
+}
+
+# Select data for filters 
+countries <-  mSqlFetch(con,"RR_GEO_UNIT")
+risknames <-  mSqlFetch(con,"RR_INDICATOR")
+riskcats  <-  levels(factor(risknames[,"INDICATOR_CATEGORY"]))
+periods   <-  mSqlFetch(con,"RR_TIME_PERIOD")
